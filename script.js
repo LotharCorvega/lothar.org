5b6162e140fcf6f444507562979691e9213ae7fb
LotharCorvega <scherer.peer@hotmail.com>
2020-09-20 00:01:32 +0200

    added directory

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-
-
-
-function show_form_login() {
-    var form_login = document.getElementById("form_login");
-    if (form_login.style.display == "none")
-      form_login.style.display = "block";
-    else
-      form_login.style.display = "none";
+function show_form_login()
+{
+  var form_login = document.getElementById("form_login");
+  if (form_login.style.display == "none")
+    form_login.style.display = "block";
+  else
+    form_login.style.display = "none";
 }
